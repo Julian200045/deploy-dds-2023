54aa330d95168ec14b3a74d537c2178ba0c1b0c6
IanStratico <straticoian@gmail.com>
2023-10-17 09:07:33 -0300

    borro algo viejo

--- TP-DDS-2023/src/main/java/controllers/FactoryController.java
+++ TP-DDS-2023/src/main/java/controllers/FactoryController.java
@@ -10,11 +10,11 @@ public class FactoryController {
 
     switch (nombre) {
       case "Incidentes": controller = new IncidentesController(new RepositorioIncidentes()); break;
-<<<<<<< HEAD
+
       case "AdminUsuarios": controller = new UsuariosController(new RepositorioUsuarios()); break;
-=======
+
       case "Inicio": controller = new InicioController(new RepositorioIncidentes()); break;
->>>>>>> e5ab56c4275182e59aabe7f15e07cddd6c3ca7df
+
     }
 
     return controller;
