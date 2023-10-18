3a9932669cd03a77b0eee9da95dd8795fb08a25b
Tomas Gomez Berruezo <tgomezberruezo@frba.utn.edu.ar>
2023-10-18 17:56:07 -0300

    reorganizo rutas

--- TP-DDS-2023/src/main/java/server/Router.java
+++ TP-DDS-2023/src/main/java/server/Router.java
@@ -13,14 +13,12 @@ public class Router {
     Server.app().routes(() -> {
       get("inicio", ((IncidentesController) FactoryController.controller("Incidentes"))::index);
       get("alta-incidente",((IncidentesController) FactoryController.controller("Incidentes"))::create);
-
-      post("incidentes",((IncidentesController) FactoryController.controller("Incidentes"))::save);
-      patch("inicio/incidentes/{id}/dar-de-baja",((IncidentesController) FactoryController.controller("Incidentes"))::update);
-
       get("administracion/usuarios", ((UsuariosController) FactoryController.controller("AdminUsuarios"))::index);
       get("administracion/usuario", ((UsuariosController) FactoryController.controller("AdminUsuarios"))::show);
       get("administracion/usuario/busqueda", ((UsuariosController) FactoryController.controller("AdminUsuarios"))::show);
+      post("incidentes",((IncidentesController) FactoryController.controller("Incidentes"))::save);
       post("administracion/usuario/{id}", ((UsuariosController) FactoryController.controller("AdminUsuarios"))::create);
+      patch("inicio/incidentes/{id}/dar-de-baja",((IncidentesController) FactoryController.controller("Incidentes"))::update);
       put("administracion/usuario", ((UsuariosController) FactoryController.controller("AdminUsuarios"))::update);
       delete("administracion/usuario", ((UsuariosController) FactoryController.controller("AdminUsuarios"))::delete);
     });
